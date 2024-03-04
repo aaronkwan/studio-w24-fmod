dc159cf59ca85eb30a0ac5d2d212215de0dbf23a
aaronkwan <123356351+aaronkwan@users.noreply.github.com>
2024-03-03 22:59:19 -0800

    Level Two: Events
    - Add campfire, footsteps, and ambience SFX.
    - Apply distance attenuation and AHDSR modulation with event emitters.
    - Implement loop regions, parameters and automation curves.
    - Add multi-instrument and event nesting.
    - Make use of scatterer and silence instruments.

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
+using FMOD.Studio;
 
 public class Sound : MonoBehaviour
 {
@@ -9,9 +10,13 @@ public class Sound : MonoBehaviour
     [SerializeField] private EventReference pickUpStick;
     [SerializeField] private EventReference dropStick;
 
-    // Event emitters:
+    [SerializeField] private EventReference footsteps;
+    private EventInstance footstepInstance;
 
-    // One shots:
+
+    [SerializeField] private EventReference ambience;
+    private EventInstance ambienceInstance;
+
     public void FlashLight()
     {
         Debug.Log("SFX: flashing light");
@@ -28,13 +33,68 @@ public class Sound : MonoBehaviour
         RuntimeManager.PlayOneShot(dropStick, transform.position);
     }
 
+    void OnEnable()
+    {
+        footstepInstance = RuntimeManager.CreateInstance(footsteps);
+        footstepInstance.start();
+
+        ambienceInstance = RuntimeManager.CreateInstance(ambience);
+        RuntimeManager.AttachInstanceToGameObject(ambienceInstance, transform);
+        ambienceInstance.start();
+    }
+    void OnDisable()
+    {
+        footstepInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        footstepInstance.release();
+
+        ambienceInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        ambienceInstance.release();
+    }
+    void FixedUpdate()
+    {
+        // Dynamic Footsteps Method:
+        footstepInstance.setParameterByName("currentSpeed", Manager.Instance.currentSpeed);
+        footstepInstance.setParameterByName("currentGround", (int) Manager.Instance.currentGround);
+
+        // Start / Stop Method:
+        //bool isMoving = (Manager.Instance.currentSpeed > 0);
+        //FMOD.Studio.PLAYBACK_STATE playbackState;
+        //footstepInstance.getPlaybackState(out playbackState);
+        //bool soundStopped = (playbackState == PLAYBACK_STATE.STOPPED);
+
+        //if (isMoving && soundStopped)
+        //{
+        //    footstepInstance.start();
+        //}
+        //else if (!isMoving && !soundStopped)
+        //{
+        //    footstepInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        //}
+
+        // Pause/Unpause Method:
+        //bool isMoving = (Manager.Instance.currentSpeed > 0);
+        //bool isPaused;
+        //footstepInstance.getPaused(out isPaused);
+
+        //if (isMoving && isPaused)
+        //{
+        //    footstepInstance.setPaused(false);
+        //}
+        //else if (!isMoving && !isPaused)
+        //{
+        //    footstepInstance.setPaused(true);
+        //}
+    }
+
     // Reverb Zones:
     public void EnterCave()
     {
         Debug.Log("SFX: enter cave");
+        ambienceInstance.setParameterByName("location", 1); // "cave"
     }
     public void ExitCave()
     {
         Debug.Log("SFX: exit cave");
+        ambienceInstance.setParameterByName("location", 0); // "field"
     }
 }
